8e9a978ab9f79b498cdc70ef578f844b695ebb0f
chaimaebantor <chaimae.youngk@gmail.com>
2022-01-03 00:06:18 +0100

    Initial commit

---
+++ src/ma/education/tp/abstarctkeyword/Test.java
@@ -0,0 +1,19 @@
+package ma.education.tp.abstarctkeyword;
+
+public class Test
+{
+    public static void main(String[] args)
+    {
+        FormeGeometrique f1 =new FormeGeometrique();
+        FormeGeometrique f2= new FormeGeoTypeA() ;
+        FormeGeometrique f3=new Triangle();
+        FormeGeometrique f4=new FormeGeometrique()
+        {
+            @Override
+            public double calculerSurface()
+            {
+                return 22;
+            }
+        };
+    }
+}
